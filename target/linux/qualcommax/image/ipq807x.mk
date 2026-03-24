29371de3cdd16f78983ce6f5be124b0eee65dc37
sdlsadiofds <thtyjytjkyukjuy4563@gmail.com>
2026-03-24 15:42:45 +0800

    ipq807x.mk

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -583,8 +583,11 @@ define Device/inseego_fg2000
     DEVICE_MODEL := Fg2000
     DEVICE_DTS_CONFIG := config@hk09
     SOC := ipq8072
-	KERNEL_SIZE := 6144k
-	DEVICE_PACKAGES := kmod-hwmon-gpiofan
-	IMAGES += factory.bin
+    KERNEL_SIZE := 6144k
+    
+    DEVICE_PACKAGES := kmod-hwmon-gpiofan kmod-ath11k-ahb ath11k-firmware-qcn5022 kmod-phy-aquantia kmod-phy-at803x kmod-usb3 kmod-usb-dwc3-qcom kmod-fs-f2fs f2fs-tools
+    
+    IMAGES += factory.bin
+    IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs
 endef
 TARGET_DEVICES += inseego_fg2000
